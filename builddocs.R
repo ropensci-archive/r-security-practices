fdc920c6a5402880c3448ef49d397c9f09699355
Steph Locke <steph@itsalocke.com>
2017-05-26 19:10:37 +0100

    Move to gitbook

--- builddocs.R
+++ builddocs.R
@@ -1,5 +1,5 @@
 library(rmarkdown)
-bookdown::render_book("handouts/index.Rmd", "bookdown::html_book", output_dir="docs")
+bookdown::render_book("handouts/index.Rmd", "bookdown::gitbook", output_dir="docs")
 # slides=list.files("pres","*.Rmd",full.names=TRUE)
 #
 # for (f in slides) render(f,output_dir = "docs")
